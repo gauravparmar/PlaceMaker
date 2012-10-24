5585b02c11f3458a0132c69946fe39dd7c62a806
vdct <vdct@laposte.net>
2012-10-24 15:17:10 +0300

    Update php/split_geofla_par_dept.php
    Ajout de l'emprise "Reste du Monde"

--- php/split_geofla_par_dept.php
+++ php/split_geofla_par_dept.php
@@ -8,8 +8,10 @@ $str_query = "SELECT code_dept,nom_dept,round(ST_XMin(g)) xmin,round(ST_XMax(g))
 							(
 								SELECT 	distinct d.code_dept,d.nom_dept,ST_Transform(ST_Envelope(d.wkb_geometry),900913) g
 								FROM	tous_departements d
+								UNION ALL
+								SELECT 'RdM','Reste du Monde',ST_Transform(ST_SetSRID(ST_MakeBox2D(ST_Point(-170,-80),ST_Point(170,80)),4326),900913)
 							)a
-							ORDER BY substr(code_dept,1,1)::integer,substr(code_dept,2,1),substr(code_dept,3,1);";
+							ORDER BY substr(code_dept,1,1),substr(code_dept,2,1),substr(code_dept,3,1);";
 
 $rq = pg_query($GLOBALS['pgc'],$str_query);
 $res_emprises = pg_fetch_all($rq);
